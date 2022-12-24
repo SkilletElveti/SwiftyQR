2d9f7cc59c7f809de718c7bd237ca3e6b77e58d8
Shubham Kamdi <shubhamvkamdi@gmail.com>
2022-12-23 20:38:48 -0800

    Initial Commit

---
+++ SwiftyQR/SwiftyQR.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyQR.h
+//  SwiftyQR
+//
+//  Created by Shubham Kamdi on 12/23/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyQR.
+FOUNDATION_EXPORT double SwiftyQRVersionNumber;
+
+//! Project version string for SwiftyQR.
+FOUNDATION_EXPORT const unsigned char SwiftyQRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyQR/PublicHeader.h>
+
+
